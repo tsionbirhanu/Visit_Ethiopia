5545458a4d1ea980a1b431bd4e8bdd654248f0f5
Dagmawit N <21dagmawitnegash@gmail.com>
2025-08-28 21:10:08 +0300

    remove special interest

--- src/app/api/request/route.ts
+++ src/app/api/request/route.ts
@@ -25,7 +25,6 @@ export async function POST(req: Request) {
       phone_number,
       traveler_number,
       time_available,
-      special_interest,
       package_selection, // array of package IDs as strings
       language_codes,    // array of language codes
       date_time,
